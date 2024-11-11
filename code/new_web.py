a34e2521243e8a288dcde5053945baeb86af4d4d
Biangbiang0321 <e1352224@u.nus.edu>
2024-11-12 05:03:51 +0800

    adjust the web showing

--- code/new_web.py
+++ code/new_web.py
@@ -5,13 +5,14 @@ from plotnine import *
 import plotly.express as px
 import streamlit as st 
 import pycountry
-def get_country_official_name(country_name):
+
+def get_ISO_A3_code(country_name):
     try:
-        # 使用 pycountry 获取国家对象
+        
         country = pycountry.countries.lookup(country_name)
-        return country.official_name if hasattr(country, 'official_name') else country.name
+        return country.alpha_3
     except LookupError:
-        return country_name
+        return None  
 
 
 st.set_page_config(layout='wide')
@@ -21,11 +22,22 @@ st.header("The History of Global Natural Gas Production")
 line_data=pd.read_csv('../data/transformed_natural_gas_production.csv')
 map_data = pd.read_excel('../data/The history of global natural gas production.xlsx',sheet_name = 0)
 world_map = gpd.read_file('../data/worldmap.geojson')
-line_data.replace('United States','United States of America')
+country_border = gpd.read_file('../data/countries.geojson')
+
 #data manupilation
 plot_map = world_map.merge(map_data , how='left' , left_on='SOVEREIGNT' , right_on='Country')
 plot_map = plot_map[['SOVEREIGNT','Cumulative production','geometry']]
-#line_data['country'] = line_data['country'].apply(get_country_official_name)
+country_border = country_border.rename(columns={'ADMIN':'country'})
+
+data = pd.read_csv('../data/tidy_gas.csv',index_col=0)
+data['ISO_A3'] = data['country'].apply(get_ISO_A3_code)
+data.loc[data['country'] == 'Russia', 'ISO_A3'] = 'RUS'
+data.loc[data['country'] == 'Turkey', 'ISO_A3'] = 'TUR'
+data.loc[data['country'] == 'Brunei', 'ISO_A3'] = 'BRN'
+data.loc[data['country'] == 'East Timor', 'ISO_A3'] = 'TLS'
+data['gas'] = pd.to_numeric(data['gas'],errors='coerce')
+line_data = data
+
 with st.sidebar:
     st.title("setting the data")
     select_form = st.selectbox("choose the visualization form",
@@ -33,36 +45,49 @@ with st.sidebar:
         "line chart"])
     if select_form in ['line chart','table']  :
         select_countries = st.multiselect('Select up to 5 countries' , line_data['country'].unique(),max_selections=5)
-        select_year = st.slider("Choose a year", 1900, 2022)
+        select_year = st.slider("Choose a year", 1900, 2022,value = 2022 )
     if select_form == 'maps' :
         show_single_year = st.checkbox('show single year')
-        select_year = st.slider("Choose a year", 1900, 2022)
+        select_year = st.slider("Choose a year", 1900, 2022,value = 2022)
 
 
 if select_form == "maps":
     if show_single_year:
         line_plot = line_data.query('Year == @select_year')
         fig = px.choropleth_mapbox(
-            geojson = world_map, data_frame = line_plot, color = "gas",
-            featureidkey="properties.SOVEREIGNT",
-            locations = "country",
+            geojson = country_border, data_frame = line_data, color = "gas",
+            featureidkey="properties.ISO_A3",
+            locations = "ISO_A3",
             mapbox_style = "carto-positron",zoom = 1,
-            color_continuous_scale = 'blues',
-            title = "world gas production by countries")
-        st.plotly_chart(fig)
+            color_continuous_scale=px.colors.sequential.Reds,
+            hover_name = "country", hover_data = {"country": False },
+            title = "Natural gas production by country, {}".format(select_year))
+        
+        fig.update_layout(
+            width=2000,  
+            height=800,  
+            legend=dict(
+                x=0,
+                y=1,
+                xanchor='left',
+                yanchor='top'
+            )
+        )
+        st.plotly_chart(fig, use_container_width=True)
+
     else:
         fig = px.choropleth_mapbox(
             geojson = plot_map, data_frame = plot_map, color = "Cumulative production",
             featureidkey="properties.SOVEREIGNT",
             locations = "SOVEREIGNT",
             mapbox_style = "carto-positron",zoom = 1,
-            color_continuous_scale = 'YlOrRd',
+            color_continuous_scale=px.colors.sequential.Reds,
             hover_name = "SOVEREIGNT", hover_data = {"SOVEREIGNT": False},
             title = "Cumulative natural gas production by country, 1900-2022")
 
         fig.update_layout(
-            width=2000,  # 设置图的宽度，单位为像素
-            height=800,  # 设置图的高度，单位为像素
+            width=2000,  
+            height=800,  
             legend=dict(
                 x=0,
                 y=1,
@@ -72,6 +97,7 @@ if select_form == "maps":
         )
         
         st.plotly_chart(fig, use_container_width=True)
+
 
 elif select_form == "line chart":
     if not select_countries:
@@ -87,14 +113,17 @@ elif select_form == "line chart":
         color="country",  
         title="Natural gas production by country, 1900-{}".format(select_year),
         labels={"gas": "Production(Ej)", "Year": "Year"})
-
+    fig2.update_layout(
+            width=2000,  
+            height=800,  
+        )
 
     st.plotly_chart(fig2)
     
 elif select_form =='table':
     line_plot = line_data[line_data['country'].isin(select_countries)]
     line_plot = line_plot.query('Year <= @select_year')
-    line_plot['diff'] = line_plot['gas'].diff()
+    line_plot = line_plot.pivot(index='Year', columns='country', values='gas').sort_values('Year',ascending=False)
     st.dataframe(line_plot)
 
 
